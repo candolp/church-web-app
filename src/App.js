50e0704ab927a36b7022923c1600aa0eeae047bc
selete69 <lilbismark@gmail.com>
2020-06-07 04:31:23 +0100

    member registration

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/header";
 import TopNavBar from "./components/TopNavBar/index";
 import Menu from "./components/Menu/index";
 import Table from "./components/Table/index";
-import Thead from './components/TableHeader/index';
+import Thead from "./components/TableHeader/index";
 import AddMember from "./components/Modal";
 
 function App() {
   const [showDialog, setShowDialog] = useState(false);
 
   function showModal(value) {
-    setShowDialog(value);}
-  
-  return (
-    <div className="App">
-      <div className="header">
-        <Header />
-      </div>
-      <div className="sideNavBar">
-        <Menu />
-      </div>
-      <div className="topNavBar">
-        <TopNavBar />
-      </div>  
+    setShowDialog(value);
+  }
 
-      <div className='tableHead'>
-        <Thead/>
+  return (
+    <div>
+      <div className="App">
+        <div className="header">
+          <Header />
+        </div>
+        <div className="sideNavBar">
+          <Menu />
+        </div>
+        <div className="topNavBar">
+          <TopNavBar />
+        </div>
+
+        <div className="tableHead">
+          <Thead />
+        </div>
+        <div className="contentDisplay">
+          <Table />
+        </div>
       </div>
-      <div className="contentDisplay">       
-        <Table />
-        <AddMember setShowDialog={showModal} showModal={showDialog}/>
+      <div>
+        <AddMember setShowDialog={showModal} showModal={showDialog} />
       </div>
     </div>
   );
